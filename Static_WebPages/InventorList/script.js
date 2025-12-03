9a54f34eacf9321e9506a98bfeacaf73b6168616
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-12-03 12:03:50 +0530

    modified.. static InventorList webpage

--- Static_WebPages/InventorList/script.js
+++ Static_WebPages/InventorList/script.js
@@ -7,43 +7,43 @@ function showInventor() {
     const data = {
         guido: {
             img: "images/guido.jpg",
-            text: "Inventor of Python.\nFocused on simplicity.\nReleased in 1991."
+            text: "Inventor of Python.\nEmphasized simplicity.\nReleased in 1991."
         },
         dennis: {
             img: "images/dennis.jpg",
-            text: "Inventor of C language.\nCo-creator of UNIX.\nSystem programming pioneer."
+            text: "Inventor of C language.\nCo-creator of UNIX.\nSystem software pioneer."
         },
         james: {
             img: "images/james.jpg",
-            text: "Inventor of Java.\nPlatform independent.\nWidely used language."
+            text: "Inventor of Java.\nPlatform independent.\nWidely used worldwide."
         },
         bjarne: {
             img: "images/bjarne.jpg",
-            text: "Inventor of C++.\nObject-oriented extension of C.\nHigh-performance language."
+            text: "Inventor of C++.\nObject-oriented.\nHigh performance."
         },
         brendan: {
             img: "images/brendan.jpg",
-            text: "Inventor of JavaScript.\nUsed for web development.\nCreated in 1995."
+            text: "Inventor of JavaScript.\nWeb scripting language.\nCreated in 1995."
         },
         linus: {
             img: "images/linus.jpg",
-            text: "Creator of Linux.\nOpen-source kernel.\nInfluential OS."
+            text: "Creator of Linux.\nOpen-source kernel.\nOS innovator."
         },
         john: {
             img: "images/john.jpg",
-            text: "Inventor of LISP.\nArtificial Intelligence pioneer.\nCoined AI term."
+            text: "Inventor of LISP.\nAI pioneer.\nCoined AI term."
         },
         niklaus: {
             img: "images/niklaus.jpg",
-            text: "Inventor of Pascal.\nStructured programming.\nEducational language."
+            text: "Inventor of Pascal.\nStructured programming.\nTeaching language."
         },
         ken: {
             img: "images/ken.jpg",
-            text: "Co-creator of UNIX.\nB language inventor.\nInfluenced C."
+            text: "Co-developer of UNIX.\nCreated B language.\nInfluenced C."
         },
         rasmus: {
             img: "images/rasmus.jpg",
-            text: "Inventor of PHP.\nServer-side scripting.\nWeb development."
+            text: "Inventor of PHP.\nServer-side scripting.\nWeb focused."
         }
     };
 
@@ -51,18 +51,21 @@ function showInventor() {
         image.src = "";
         details.innerText = "";
         imageBox.style.borderRadius = "50%";
-        imageBox.classList.remove("glow");
+        imageBox.classList.remove("hover-effect");
         return;
     }
 
     image.src = data[select].img;
     details.innerText = data[select].text;
 
+    //  James Gosling → square + glow + hover
     if (select === "james") {
-        imageBox.style.borderRadius = "0";     // Square
-        imageBox.classList.add("glow");        // Glow
-    } else {
-        imageBox.style.borderRadius = "50%";  // Circle
-        imageBox.classList.remove("glow");    // No glow
+        imageBox.style.borderRadius = "0";
+        imageBox.classList.add("hover-effect");
+    } 
+    // Others → circle only
+    else {
+        imageBox.style.borderRadius = "50%";
+        imageBox.classList.remove("hover-effect");
     }
 }
